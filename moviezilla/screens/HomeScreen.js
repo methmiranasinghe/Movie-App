23545c9fece80e2bc604f854d354270f1a03e3e2
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-01-14 19:22:47 +0530

    initial commit

---
+++ moviezilla/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { View , Text} from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+
+
+export default function HomeScreen(){
+    return(
+        <SafeAreaView>
+            <Text>
+                HomeScreen
+            </Text>
+        </SafeAreaView>
+
+    );
+
+
+
+}
